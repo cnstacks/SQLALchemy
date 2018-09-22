aa047f53909771861eddb6318109741a82177f66
tqtl911@163.com <19930911cXS>
2018-09-22 08:19:59 +0800

    metaclass

--- 1.面向对象相关/3.metaclass.py
+++ 1.面向对象相关/3.metaclass.py
@@ -22,11 +22,11 @@ def func(self, x):
     return x + 1
 
 
-#Foo1 = type('Foo1', (object,), {'CITY': 'BJ', 'func': func})
+# Foo1 = type('Foo1', (object,), {'CITY': 'BJ', 'func': func})
 
 
 # 另外一种形式:
-Foo2 = type('Foo', (object,), {'CITY': 'BJ','func':lambda self,x:x+1})
+Foo2 = type('Foo', (object,), {'CITY': 'BJ', 'func': lambda self, x: x + 1})
 
 
 # 2、类由自定义type创建；
@@ -50,25 +50,39 @@ class Foo4(object, metaclass=MyType):
 
     def func(self, x):
         return x + 1
+
+
 print(Foo4)
 """
 1、类由type创建，metaclass可以指定当前类由哪一个类创建;
 """
 
 
-
-
 class MyType1(type):
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         print('创建类之前')
-        super(MyType1,self).__init__(*args,**kwargs)
+        super(MyType1, self).__init__(*args, **kwargs)
         print('创建类之后')
 
 
-class Foo1(object,metaclass=MyType1):
+class Foo1(object, metaclass=MyType1):
     CITY = 'BJ'
-    def func(self,x):
-        return x +1
+
+    def func(self, x):
+        return x + 1
+
 
 class Bar(Foo):
     pass
+
+
+"""
+小结：
+1、默认类由type实例化创建；
+2、某个类指定metaclass = MyType，那么当前类的所有派生类都由于MyType创建；
+3、实例化对象：
+    -type.__init__
+    -type.__call__
+    -类名.__new__
+    -类名.__init__
+"""
